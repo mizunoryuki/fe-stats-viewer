7590d3233ded1f5054e917556cfeacc6afe33f67
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-02-08 08:39:26 +0900

    get repositories info

--- scan.ts
+++ scan.ts
@@ -1,6 +1,29 @@
+import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+async function main() {
+  // リポジトリ一覧取得
+  let page = 1;
+  const allRepos: RestEndpointMethodTypes["repos"]["listForAuthenticatedUser"]["response"]["data"] =
+    [];
+  while (true) {
+    const { data } = await octokit.rest.repos.listForAuthenticatedUser({
+      per_page: 100,
+      page: page,
+      affiliation: "owner",
+    });
+    if (data.length === 0) break;
+    allRepos.push(...data);
+    page++;
+    console.log("pushed");
+  }
+  allRepos.map((data) => console.log(data.name))
+}
+
+main();
