7e6c12cf02579cfae040b54b327ca7572e17c364
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-02-08 08:16:49 +0900

    install prettier

--- scan.ts
+++ scan.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
